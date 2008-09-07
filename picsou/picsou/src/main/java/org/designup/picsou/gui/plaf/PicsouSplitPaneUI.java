4a6ebde02ebaf23f8626e283332379b4be36baba
rmedina <devnull@localhost>
2008-09-07 10:46:50 +0200

    Redefinition look PicsouSplitPaneUI

--- picsou/picsou/src/main/java/org/designup/picsou/gui/plaf/PicsouSplitPaneUI.java
+++ picsou/picsou/src/main/java/org/designup/picsou/gui/plaf/PicsouSplitPaneUI.java
@@ -1,14 +1,43 @@
 package org.designup.picsou.gui.plaf;
 
-import javax.swing.plaf.basic.BasicSplitPaneDivider;
+import javax.swing.*;
 import javax.swing.plaf.basic.BasicSplitPaneUI;
 import java.awt.*;
 
 public class PicsouSplitPaneUI extends BasicSplitPaneUI {
-  public BasicSplitPaneDivider getDivider() {
-    return new BasicSplitPaneDivider(this) {
-      public void paint(Graphics g) {
-      }
-    };
+  private Color handleColor = Color.WHITE;
+  private int handleSize = 30;
+
+  public void setHandleColor(Color handleColor) {
+    this.handleColor = handleColor;
+  }
+
+  public void setHandleSize(int handleSize) {
+    this.handleSize = handleSize;
+  }
+
+  public void installUI(JComponent component) {
+    super.installUI(component);
+
+    JSplitPane splitPane = (JSplitPane)component;
+    splitPane.setBorder(null);
+  }
+
+  public void paint(Graphics g, JComponent component) {
+    Graphics2D g2 = (Graphics2D)g;
+    JSplitPane splitPane = (JSplitPane)component;
+
+    g2.setPaint(handleColor);
+    if (splitPane.getOrientation() == JSplitPane.VERTICAL_SPLIT) {
+      int handleX = component.getWidth() / 2 - handleSize / 2;
+      int handleY = splitPane.getDividerLocation() + splitPane.getDividerSize() / 2;
+      g2.fillRect(handleX, handleY, handleSize, 1);
+    }
+    else {
+      int handleX = splitPane.getDividerLocation() + splitPane.getDividerSize() / 2;
+      int handleY = component.getHeight() / 2 - handleSize / 2;
+      g2.fillRect(handleX, handleY, 1, handleSize);
+
+    }
   }
 }
---
+++ picsou/picsou/src/test/java/org/designup/picsou/gui/plaf/PicsouSplitPaneUIDemo.java
@@ -0,0 +1,41 @@
+package org.designup.picsou.gui.plaf;
+
+import org.designup.picsou.gui.utils.Gui;
+import org.globsframework.gui.splits.IconLocator;
+import org.globsframework.gui.splits.SplitsBuilder;
+import org.globsframework.gui.splits.color.ColorService;
+import org.globsframework.gui.splits.ui.UIService;
+import org.globsframework.gui.splits.utils.GuiUtils;
+import org.globsframework.utils.directory.DefaultDirectory;
+
+import javax.swing.*;
+
+public class PicsouSplitPaneUIDemo {
+  public static void main(String[] args) {
+    DefaultDirectory directory = new DefaultDirectory();
+    directory.add(new ColorService());
+    directory.add(new UIService());
+    directory.add(IconLocator.class, Gui.ICON_LOCATOR);
+
+    SplitsBuilder builder = new SplitsBuilder(directory);
+    builder.setSource("<splits>" +
+                      "  <styles>" +
+                      "    <ui name='ui' class='" + PicsouSplitPaneUI.class.getName() + "'" +
+                      "        handleColor='#FF0000'/>" +
+                      "    <style selector='horizontalSplit' ui='ui'/>" +
+                      "  </styles>" +
+                      "<panel background='#1e64c2' opaque='true'>" +
+                      "  <row margin='15' preferredSize='(500,500)'>" +
+                      "    <horizontalSplit dividerLocation='200' opaque='false'>" +
+                      "      <list/>" +
+                      "      <table/>" +
+                      "    </horizontalSplit>" +
+                      "  </row>" +
+                      "</panel>" +
+                      "</splits>");
+
+    JPanel panel = builder.load();
+    GuiUtils.show(panel);
+  }
+
+}
