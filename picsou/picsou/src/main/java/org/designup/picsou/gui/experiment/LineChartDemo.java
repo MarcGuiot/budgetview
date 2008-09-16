4f14878695f3d7d8aaa05754b4c380a96edda8aa
rmedina <devnull@localhost>
2008-09-16 10:10:59 +0200

    joliesse

---
+++ picsou/picsou/src/main/java/org/designup/picsou/gui/experiment/LineChartDemo.java
@@ -0,0 +1,23 @@
+package org.designup.picsou.gui.experiment;
+
+import org.designup.picsou.gui.components.LineChart;
+import org.globsframework.gui.splits.layout.GridBagBuilder;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class LineChartDemo {
+
+  private static final double[] VALUES = {2, 0, 8, 5, -10, 10, 4};
+
+  public static void main(String[] args) {
+
+    LineChart chart = new LineChart();
+    chart.setValues(VALUES);
+
+    JFrame frame = new JFrame();
+    frame.setContentPane(GridBagBuilder.createSingleCell(chart, new Insets(20, 20, 20, 20)));
+    frame.setSize(new Dimension(1200, 500));
+    frame.setVisible(true);
+  }
+}
