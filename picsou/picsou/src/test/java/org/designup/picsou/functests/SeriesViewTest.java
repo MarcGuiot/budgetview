ea359d685a17d7ce969ffded84092d1af71128d8
rmedina <devnull@localhost>
2008-09-23 15:57:14 +0200

    SeriesView : test de expand/collapse

--- picsou/picsou/src/test/java/org/designup/picsou/functests/SeriesViewTest.java
+++ picsou/picsou/src/test/java/org/designup/picsou/functests/SeriesViewTest.java
@@ -5,7 +5,8 @@ import org.designup.picsou.functests.utils.OfxBuilder;
 import org.designup.picsou.model.MasterCategory;
 
 public class SeriesViewTest extends LoggedInFunctionalTestCase {
-  public void test() throws Exception {
+
+  public void testStandardUsage() throws Exception {
     OfxBuilder.init(this)
       .addTransaction("2008/07/12", -95.00, "Auchan")
       .addTransaction("2008/07/10", -50.00, "Monoprix")
@@ -37,19 +38,105 @@ public class SeriesViewTest extends LoggedInFunctionalTestCase {
     views.selectData();
     series.checkContains("All",
                          "To categorize",
-                         "Income","Exceptional Income","Salary",
-                         "Recurring expenses","Electricity","Internet",
-                         "Envelope expenses","Groceries",
+                         "Income", "Exceptional Income", "Salary",
+                         "Recurring expenses", "Electricity", "Internet",
+                         "Envelope expenses", "Groceries",
+                         "Occasional expenses",
+                         "Projects",
+                         "Savings");
+
+  }
+
+  public void testCreatingAndDeletingSeries() throws Exception {
+    OfxBuilder.init(this)
+      .addTransaction("2008/07/12", -95.00, "Auchan")
+      .load();
+
+    views.selectBudget();
+    budgetView.envelopes.createSeries()
+      .setName("New envelope")
+      .setCategory(MasterCategory.FOOD)
+      .validate();
+
+    views.selectData();
+    series.checkContains("All",
+                         "To categorize",
+                         "Income",
+                         "Recurring expenses",
+                         "Envelope expenses", "New envelope",
+                         "Occasional expenses",
+                         "Projects",
+                         "Savings");
+
+    views.selectBudget();
+    budgetView.envelopes.editSeriesList()
+      .selectSeries("New envelope")
+      .deleteSeries()
+      .validate();
+
+    views.selectData();
+    series.checkContains("All",
+                         "To categorize",
+                         "Income",
+                         "Recurring expenses",
+                         "Envelope expenses",
                          "Occasional expenses",
                          "Projects",
                          "Savings");
   }
 
-  public void testCreatingAndDeletingSeries() throws Exception {
-
-  }
-
   public void testExpandingAndCollapsingBudgetAreas() throws Exception {
+    OfxBuilder.init(this)
+      .addTransaction("2008/07/12", -95.00, "Auchan")
+      .load();
+
+    views.selectData();
+    series.checkContains("All",
+                         "To categorize",
+                         "Income",
+                         "Recurring expenses",
+                         "Envelope expenses",
+                         "Occasional expenses",
+                         "Projects",
+                         "Savings");
+
+    views.selectBudget();
+    budgetView.envelopes.createSeries()
+      .setName("envelope1")
+      .setCategory(MasterCategory.FOOD)
+      .validate();
+
+    budgetView.envelopes.createSeries()
+      .setName("envelope2")
+      .setCategory(MasterCategory.LEISURES)
+      .validate();
+
+    views.selectData();
+    series.checkContains("All",
+                         "To categorize",
+                         "Income",
+                         "Recurring expenses",
+                         "Envelope expenses", "envelope1", "envelope2",
+                         "Occasional expenses",
+                         "Projects",
+                         "Savings");
+
+    series.checkExpansionEnabled("All", false);
+    series.checkExpansionEnabled("To categorize", false);
+    series.checkExpansionEnabled("Income", false);
+
+    series.checkExpansionEnabled("Envelope expenses", true);
+    series.toggle("Envelope expenses");
+
+    series.checkContains("All",
+                         "To categorize",
+                         "Income",
+                         "Recurring expenses",
+                         "Envelope expenses",
+                         "Occasional expenses",
+                         "Projects",
+                         "Savings");
+
 
   }
 }
--- picsou/picsou/src/test/java/org/designup/picsou/functests/checkers/SeriesViewChecker.java
+++ picsou/picsou/src/test/java/org/designup/picsou/functests/checkers/SeriesViewChecker.java
@@ -1,9 +1,14 @@
 package org.designup.picsou.functests.checkers;
 
-import org.uispec4j.Window;
-import org.uispec4j.Table;
-import org.uispec4j.assertion.UISpecAssert;
+import junit.framework.Assert;
+import org.designup.picsou.gui.components.expansion.TableExpansionColumn;
 import org.designup.picsou.gui.series.view.SeriesView;
+import org.uispec4j.Button;
+import org.uispec4j.Table;
+import org.uispec4j.Window;
+import org.uispec4j.assertion.UISpecAssert;
+
+import javax.swing.*;
 
 public class SeriesViewChecker extends DataChecker {
   private Window mainWindow;
@@ -14,6 +19,29 @@ public class SeriesViewChecker extends DataChecker {
 
   public void checkContains(String... items) {
     UISpecAssert.assertThat(getTable().columnEquals(SeriesView.LABEL_COLUMN_INDEX, items));
+  }
+
+  public void select(String label) {
+    Table table = getTable();
+    int row = table.getRowIndex(SeriesView.LABEL_COLUMN_INDEX, label);
+    table.selectRow(row);
+  }
+
+  public void checkExpansionEnabled(String label, boolean enabled) {
+    Table table = getTable();
+    int row = table.getRowIndex(SeriesView.LABEL_COLUMN_INDEX, label);
+    JButton button = (JButton)getTable().getSwingRendererComponentAt(row, 0);
+    Assert.assertEquals(enabled,
+                        (button.getIcon() != null) &&
+                        (button.getIcon() != TableExpansionColumn.DISABLED_ICON));
+  }
+
+  public void toggle(String label) {
+    Table table = getTable();
+    int row = table.getRowIndex(SeriesView.LABEL_COLUMN_INDEX, label);
+    table.selectRow(row);
+    JButton button = (JButton)getTable().getSwingEditorComponentAt(row, 0);
+    new Button(button).click();
   }
 
   private Table getTable() {
