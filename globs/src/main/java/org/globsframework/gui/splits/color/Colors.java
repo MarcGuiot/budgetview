98afc6add364d2ff2a8f4ebc793b59ca83cb21f6
rmedina <devnull@localhost>
2009-09-12 19:09:23 +0200

    Colors class: added brighten() + getLabelColor() + getLuminance()

--- globs/src/main/java/org/globsframework/gui/splits/color/Colors.java
+++ globs/src/main/java/org/globsframework/gui/splits/color/Colors.java
@@ -1,9 +1,12 @@
 package org.globsframework.gui.splits.color;
 
+import org.globsframework.utils.exceptions.InvalidParameter;
+
 import java.awt.*;
 
 public class Colors {
   public static final String HEXA_PREFIX = "#";
+  private static final int LABEL_SWITCH_LIMIT = 180;
 
   private Colors() {
   }
@@ -27,5 +30,40 @@ public class Colors {
 
   public static boolean isHexaString(String text) {
     return text.startsWith(Colors.HEXA_PREFIX);
+  }
+
+  public static Color brighten(Color color, float factor) {
+    if ((factor < 0) || (factor > 1.0)) {
+      throw new InvalidParameter("Invalid factor " + factor + " - should be between 0.0 and 1.0");
+    }
+    if (Math.abs(1 - factor) < 0.05) {
+      return color;
+    }
+    int red = color.getRed();
+    int green = color.getGreen();
+    int blue = color.getBlue();
+
+    int adjustedRed = brigthen(factor, red);
+    int adjustedGreen = brigthen(factor, green);
+    int adjustedBlue = brigthen(factor, blue);
+
+    return new Color(adjustedRed, adjustedGreen, adjustedBlue);
+  }
+
+  private static int brigthen(float factor, int color) {
+    float distance = factor * (255 - color);
+    return (int)(color + distance);
+  }
+
+  public static  Color getLabelColor(Color background, Color lightLabelColor, Color darkLabelColor) {
+    int luminance = getLuminance(background);
+    return luminance < LABEL_SWITCH_LIMIT ? lightLabelColor : darkLabelColor;
+  }
+
+  public static int getLuminance(Color background) {
+    int red = background.getRed();
+    int green = background.getGreen();
+    int blue = background.getBlue();
+    return (int)(0.2126 * red + 0.7152 * blue + 0.0722 * green);
   }
 }
