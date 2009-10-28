ce161056ccea76ada06dc4d4aa952bea97d129bd
mguiot <devnull@localhost>
2009-10-28 21:17:51 +0100

    correction retreiveJar

--- picsou/picsou_loader/dev/install/retreiveJar.sh
+++ picsou/picsou_loader/dev/install/retreiveJar.sh
@@ -22,7 +22,7 @@ fi
 
 JAR_VERSION=$1
 
-scp build@91.121.123.100:version/cashpilot${JAR_VERSION}.jar ../picsou/obfuscated/cashpilot.jar
-scp build@91.121.123.100:version/ChangeLogOutput-${JAR_VERSION}.txt.bz2 ../picsou/
+scp build@91.121.123.100:versions/cashpilot${JAR_VERSION}.jar ../picsou/obfuscated/cashpilot.jar
+scp build@91.121.123.100:versions/ChangeLogOutput-${JAR_VERSION}.txt.bz2 ../picsou/
 
 
