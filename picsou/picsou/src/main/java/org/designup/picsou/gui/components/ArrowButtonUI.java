434a938e321030b2e0c0001f4f943f1a0e965541
rmedina <devnull@localhost>
2009-12-29 11:58:40 +0100

    Bug Laurent / bouton 'Projets & Epargne' masque par moments

--- picsou/picsou/src/main/java/org/designup/picsou/gui/components/ArrowButtonUI.java
+++ picsou/picsou/src/main/java/org/designup/picsou/gui/components/ArrowButtonUI.java
@@ -11,6 +11,8 @@ public class ArrowButtonUI extends BasicButtonUI {
 
   private boolean doubleArrow = false;
   private int rotation = 0;
+  private Dimension shapeSize;
+
   private Color pressedColor = Color.GRAY;
   private Color rolloverColor = Color.BLUE;
   private Color disabledColor = Color.DARK_GRAY;
@@ -20,29 +22,54 @@ public class ArrowButtonUI extends BasicButtonUI {
     JButton button = (JButton)c;
     button.setOpaque(false);
     button.setBorderPainted(false);
+    button.setBorder(null);
     button.setRolloverEnabled(true);
+    if (shapeSize == null) {
+      shapeSize = button.getPreferredSize();
+    }
   }
 
-  public void paint(Graphics g, JComponent c) {
-    super.paint(g, c);
-
-    GeneralPath shape = createTriangleShape();
-
-    int width = c.getWidth() - 1;
-    int height = c.getHeight() - 1;
-
-    Rectangle initialRectangle = shape.getBounds();
-    float ratio = height / (float)initialRectangle.height;
-    AffineTransform scaling =
-      AffineTransform.getScaleInstance(ratio, ratio);
-    shape.transform(scaling);
-
-    Rectangle rectangle = shape.getBounds();
-    float middleY = (float)height / 2;
+  public void paint(Graphics g, JComponent button) {
+    super.paint(g, button);
 
     Graphics2D g2 = (Graphics2D)g;
     g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+    initColor(g2, button);
 
+    int shapeHeight = shapeSize.height;
+
+    int areaWidth = button.getWidth() - 1;
+    int areaHeight = button.getHeight() - 1;
+
+    GeneralPath shape = createTriangleShape();
+    Rectangle initialRectangle = shape.getBounds();
+    float ratio = shapeHeight / (float)initialRectangle.height;
+    AffineTransform scaling = AffineTransform.getScaleInstance(ratio, ratio);
+    shape.transform(scaling);
+
+    Rectangle rectangle = shape.getBounds();
+    float middleY = (float)areaHeight / 2;
+
+    if (rotation != 0) {
+      shape.transform(AffineTransform.getRotateInstance(Math.PI, rectangle.width / 2, rectangle.height / 2));
+    }
+
+    if (doubleArrow) {
+      shape.transform(getTranslateInstance((areaWidth - rectangle.width) / 2 - rectangle.width / 2,
+                                           middleY - rectangle.height / 2 - initialRectangle.y));
+
+      g2.fill(shape);
+      shape.transform(getTranslateInstance(rectangle.width, 0));
+      g2.fill(shape);
+    }
+    else {
+      shape.transform(getTranslateInstance((areaWidth - rectangle.width) / 2,
+                                           middleY - rectangle.height / 2 - initialRectangle.y));
+      g2.fill(shape);
+    }
+  }
+
+  private void initColor(Graphics2D g2, JComponent c) {
     JButton button = (JButton)c;
     ButtonModel model = button.getModel();
     if (!model.isEnabled()) {
@@ -56,23 +83,6 @@ public class ArrowButtonUI extends BasicButtonUI {
     }
     else {
       g2.setColor(c.getForeground());
-    }
-
-    if (rotation != 0)
-    shape.transform(AffineTransform.getRotateInstance(Math.PI, rectangle.width / 2, rectangle.height / 2));
-
-    if (doubleArrow) {
-      shape.transform(getTranslateInstance((width - rectangle.width) / 2 - rectangle.width / 2,
-                                           middleY - height / 2 - initialRectangle.y));
-
-      g2.fill(shape);
-      shape.transform(getTranslateInstance(rectangle.width, 0));
-      g2.fill(shape);
-    }
-    else {
-      shape.transform(getTranslateInstance((width - rectangle.width) / 2,
-                                           middleY - height / 2 - initialRectangle.y));
-      g2.fill(shape);
     }
   }
 
@@ -103,5 +113,9 @@ public class ArrowButtonUI extends BasicButtonUI {
 
   public void setRotation(int degrees) {
     this.rotation = degrees;
+  }
+
+  public void setSize(Dimension size) {
+    this.shapeSize = size;
   }
 }
