7236991c6435f235d2c8093c08ca4b525345dd21
mguiot <devnull@localhost>
2010-03-02 18:44:34 +0100

    BudgetSummaryView : on appel updateHelpMessage sur GlobReset meme si il n'y a pas de transaction

--- picsou/picsou/src/main/java/org/designup/picsou/gui/budget/summary/BudgetSummaryView.java
+++ picsou/picsou/src/main/java/org/designup/picsou/gui/budget/summary/BudgetSummaryView.java
@@ -111,6 +111,7 @@ public class BudgetSummaryView extends View implements GlobSelectionListener, Ch
       clear(balanceLabel);
       clear(estimatedPositionLabel);
       clear(uncategorizedButton);
+      updateHelpMessage();
       return;
     }
 
