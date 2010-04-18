4c43ddf848e740026555ec0991da2d2057ec3844
mguiot <devnull@localhost>
2010-04-18 23:07:11 +0200

    Version 0.42

--- picsou/picsou/src/main/java/org/designup/picsou/gui/PicsouApplication.java
+++ picsou/picsou/src/main/java/org/designup/picsou/gui/PicsouApplication.java
@@ -47,8 +47,8 @@ import java.util.regex.Pattern;
 
 public class PicsouApplication {
 
-  public static final String APPLICATION_VERSION = "0.41";
-  public static final Long JAR_VERSION = 38L;
+  public static final String APPLICATION_VERSION = "0.42";
+  public static final Long JAR_VERSION = 39L;
   public static final Long BANK_CONFIG_VERSION = 7L;
   private static final String JAR_DIRECTORY = "jars";
   private static final String BANK_CONFIG_DIRECTORY = "configs";
