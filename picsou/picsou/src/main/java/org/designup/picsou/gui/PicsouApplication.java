9c8f926d2e6f8eadf09df5d0ba8c3a84e7b9f6de
rmedina <devnull@localhost>
2010-05-09 15:12:27 +0200

    Version 0.44

--- picsou/picsou/src/main/java/org/designup/picsou/gui/PicsouApplication.java
+++ picsou/picsou/src/main/java/org/designup/picsou/gui/PicsouApplication.java
@@ -47,8 +47,8 @@ import java.util.regex.Pattern;
 
 public class PicsouApplication {
 
-  public static final String APPLICATION_VERSION = "0.43";
-  public static final Long JAR_VERSION = 40L;
+  public static final String APPLICATION_VERSION = "0.44";
+  public static final Long JAR_VERSION = 41L;
   public static final Long BANK_CONFIG_VERSION = 7L;
   private static final String JAR_DIRECTORY = "jars";
   private static final String BANK_CONFIG_DIRECTORY = "configs";
