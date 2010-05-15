5c006dbc448ce839b350f69f5bb48b7e5f51f8f2
mguiot <devnull@localhost>
2010-05-15 14:11:26 +0200

    Correction le mesage apparaisait toujours dans importDialog Inversion dans importDialguStep1.splits des deux entrées.

--- picsou/picsou/src/test/java/org/designup/picsou/functests/ImportTest.java
+++ picsou/picsou/src/test/java/org/designup/picsou/functests/ImportTest.java
@@ -23,6 +23,7 @@ public class ImportTest extends LoggedInFunctionalTestCase {
       .save();
 
     operations.openImportDialog()
+      .checkNoErrorMessage()
       .checkHeaderMessage("Select an OFX or QIF file to import")
       .acceptFile()
       .checkErrorMessage("login.data.file.required")
