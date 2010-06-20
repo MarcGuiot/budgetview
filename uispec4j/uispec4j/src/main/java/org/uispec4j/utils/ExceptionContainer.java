2e00963e7aec149c6198a3b207846aca4fbc4af8
mguiot <devnull@localhost>
2010-06-20 16:57:53 +0200

    Prevent lose of stackstrace.

--- uispec4j/uispec4j/src/main/java/org/uispec4j/utils/ExceptionContainer.java
+++ uispec4j/uispec4j/src/main/java/org/uispec4j/utils/ExceptionContainer.java
@@ -1,9 +1,14 @@
 package org.uispec4j.utils;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ExceptionContainer {
   private RuntimeException exception;
   private Error error;
   private StackTraceElement[] stackTraceElements;
+  private Set<Throwable> registered = new HashSet<Throwable>();
+  private Throwable throwable = null;
 
   public ExceptionContainer() {
   }
@@ -13,6 +18,28 @@ public class ExceptionContainer {
   }
 
   public void set(Throwable e) {
+    if (!registered.add(e)) {
+      return;
+    }
+    {
+      Throwable subException = e;
+      boolean found = false;
+      while (subException.getCause() != null) {
+        subException = subException.getCause();
+        found |= !registered.add(subException);
+      }
+      if (found){
+        return;
+      }
+    }
+
+    if (throwable != null) {
+      Throwable subException = e;
+      while (subException.getCause() != null) {
+        subException = subException.getCause();
+      }
+      subException.initCause(throwable);
+    }
     if (e instanceof RuntimeException) {
       exception = (RuntimeException)e;
     }
@@ -22,6 +49,7 @@ public class ExceptionContainer {
     else {
       exception = new RuntimeException(e);
     }
+    throwable = e;
   }
 
   public boolean isSet() {
@@ -51,7 +79,7 @@ public class ExceptionContainer {
   private void completeStackTrace(Throwable exception) {
     StackTraceElement[] stackTraceElements1 = exception.getStackTrace();
     StackTraceElement[] newStack = new StackTraceElement[stackTraceElements.length + stackTraceElements1.length];
-    int i =0;
+    int i = 0;
     for (StackTraceElement element : stackTraceElements) {
       newStack[i] = element;
       i++;
