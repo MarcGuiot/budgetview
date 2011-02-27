4a07453b90b13a88cd03e2d94eac38930fe541f8
mguiot <devnull@localhost>
2011-02-27 10:20:20 +0100

    bad import

--- picsou/picsou/src/main/java/org/designup/picsou/client/http/MD5PasswordBasedEncryptor.java
+++ picsou/picsou/src/main/java/org/designup/picsou/client/http/MD5PasswordBasedEncryptor.java
@@ -1,32 +1,18 @@
 package org.designup.picsou.client.http;
 
+import org.globsframework.utils.Log;
 import org.globsframework.utils.exceptions.GlobsException;
 import org.globsframework.utils.exceptions.UnexpectedApplicationState;
-import org.globsframework.utils.serialization.SerializedInput;
-import org.globsframework.utils.MapOfMaps;
-import org.globsframework.utils.Log;
-import org.globsframework.model.GlobList;
-import org.globsframework.model.utils.GlobBuilder;
-import org.globsframework.metamodel.GlobModel;
-import org.globsframework.metamodel.GlobType;
-import org.globsframework.metamodel.fields.IntegerField;
-import org.designup.picsou.client.ServerAccess;
-import org.designup.picsou.client.SerializableGlobSerializer;
-import org.designup.picsou.server.model.SerializableGlobType;
-import org.designup.picsou.server.serialization.PicsouGlobSerializer;
-import org.designup.picsou.server.serialization.SerializationManager;
-import org.uispec4j.utils.Chrono;
 
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.PBEParameterSpec;
-import java.security.spec.InvalidKeySpecException;
 import java.security.NoSuchAlgorithmException;
-import java.util.Map;
+import java.security.spec.InvalidKeySpecException;
 
-public class MD5PasswordBasedEncryptor implements PasswordBasedEncryptor{
+public class MD5PasswordBasedEncryptor implements PasswordBasedEncryptor {
   private SecretKey secretKey;
   private Cipher cipher;
   private PBEParameterSpec spec;
@@ -56,12 +42,12 @@ public class MD5PasswordBasedEncryptor implements PasswordBasedEncryptor{
   }
 
   public synchronized static void init() throws NoSuchAlgorithmException {
-    if (factory == null){
+    if (factory == null) {
       long start = System.currentTimeMillis();
       factory = SecretKeyFactory.getInstance("PBEWithMD5AndDES");
       long stop = System.currentTimeMillis();
       long duration = stop - start;
-      if (duration > 1000){
+      if (duration > 1000) {
         Log.write("Take too long to compute MD5 : " + duration + "ms");
       }
     }
