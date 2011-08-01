f8d318977bc2008f02fda6d543a79ac2d0b62be4
rmedina <devnull@localhost>
2011-08-01 21:13:35 +0200

    Added labels to ToggleVisibilityAction

--- globs/src/main/java/org/globsframework/gui/splits/utils/ToggleVisibilityAction.java
+++ globs/src/main/java/org/globsframework/gui/splits/utils/ToggleVisibilityAction.java
@@ -6,12 +6,26 @@ import java.awt.event.ActionEvent;
 public class ToggleVisibilityAction extends AbstractAction {
 
   private JComponent component;
+  private String shownLabel;
+  private String hiddenLabel;
 
   public ToggleVisibilityAction(JComponent component) {
+    this(component, null, null);
+  }
+
+  public ToggleVisibilityAction(JComponent component, String shownLabel, String hiddenLabel) {
     this.component = component;
+    this.shownLabel = shownLabel;
+    this.hiddenLabel = hiddenLabel;
+    updateLabel();
+  }
+
+  private void updateLabel() {
+    putValue(Action.NAME, component.isVisible() ? shownLabel : hiddenLabel);
   }
 
   public void actionPerformed(ActionEvent e) {
     component.setVisible(!component.isVisible());
+    updateLabel();
   }
 }
