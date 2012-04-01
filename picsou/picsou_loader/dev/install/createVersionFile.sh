a1ecc5127ea88a4321ddacbc3badfb51aa53b751
mguiot <devnull@localhost>
2012-04-01 21:11:33 +0200

    add version in dmg

---
+++ picsou/picsou_loader/dev/install/createVersionFile.sh
@@ -0,0 +1,4 @@
+SOFT_VERSION=`java -jar ../picsou/obfuscated/budgetview.jar -v -soft | grep "Software version:" |
+              sed -e 's/Software version://g' | sed -e 's/  *//g'`
+
+echo "budgetviewVersion="${SOFT_VERSION} > budgetviewVersion.properties
