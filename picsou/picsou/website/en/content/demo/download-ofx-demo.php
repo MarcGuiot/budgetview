8d72dda3c2fa0a99cbb784112f09cf3197cc61ee
rmedina <devnull@localhost>
2012-05-06 18:01:05 +0200

    Chgt page 'BudgetView demo bank' + download OFX

--- picsou/picsou/website/en/content/demo/download-ofx-demo.php
+++
@@ -1,6 +0,0 @@
-<?php
-header('Content-Description: File Transfer');
-header('Content-disposition: attachment; filename=demo.ofx');
-header('Content-type: text/ofx');
-readfile('/files/demo.ofx');
-?>
