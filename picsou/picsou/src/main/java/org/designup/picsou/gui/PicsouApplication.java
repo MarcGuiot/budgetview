3fd1679116476d1b0b3ead3168c25b7ffd305430
mguiot <devnull@localhost>
2013-06-25 20:39:56 +0200

    Version:3.07 jar:121 Protection en cas d'incoherence des données des operations miroir.

--- picsou/picsou/src/main/java/org/designup/picsou/gui/PicsouApplication.java
+++ picsou/picsou/src/main/java/org/designup/picsou/gui/PicsouApplication.java
@@ -56,8 +56,8 @@ import java.util.regex.Pattern;
 
 public class PicsouApplication {
 
-  public static final String APPLICATION_VERSION = "3.06";
-  public static Long JAR_VERSION = 120L; // not final for test
+  public static final String APPLICATION_VERSION = "3.07";
+  public static Long JAR_VERSION = 121L; // not final for test
   public static final Long BANK_CONFIG_VERSION = 7L;
 
   public static final String APPNAME = "budgetview";
