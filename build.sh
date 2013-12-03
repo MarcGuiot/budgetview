d66d13cfa8d217876e27750f5c24c40f833324b0
mguiot <devnull@localhost>
2013-12-03 23:56:32 +0100

    version:3.12 jar:130

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
+if [ "$1" == "demo" ]; then
+
 export JAVA_HOME=/opt/sun-jdk-1.6.0.45
 mvn clean install -Dmaven.test.skip.exec=true -am -pl picsou/picsou -Pgen-demo
+fi;
 
+if [ "$1" == "version" ]; then
 export JAVA_HOME=/opt/jdk1.5.0_22
 mvn clean install -Dmaven.test.skip.exec=true -Pgen-version
+fi;
