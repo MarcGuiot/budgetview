99e28f55e32e965b82240be801943ef861889c68
mguiot <devnull@localhost>
2014-01-06 23:45:41 +0100

    update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
-if [ "$1" == "demo" ]; then
-
+if [ $# -eq 0 ] || [ "$1" = "demo" ]; then
+  if [ `hg qnext` == "uispec" ]; then
+    hg qpush
+  fi;
 export JAVA_HOME=/opt/sun-jdk-1.6.0.45
 mvn clean install -Dmaven.test.skip.exec=true -am -pl picsou/picsou -Pgen-demo
 fi;
 
-if [ "$1" == "version" ]; then
+if [ $# -eq 0 ] || [ "$1" == "version" ]; then
+   if hg identify | grep uispec; then
+      hg qpop
+   fi;
 export JAVA_HOME=/opt/jdk1.5.0_22
 mvn clean install -Dmaven.test.skip.exec=true -Pgen-version
 fi;
