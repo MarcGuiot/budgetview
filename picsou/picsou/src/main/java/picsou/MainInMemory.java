7f16ee2fa1d6cf5e6c5199e7b56b01d5d528b1c5
mguiot <devnull@localhost>
2014-06-05 07:52:36 +0200

    version:3.14a jar:134

--- picsou/picsou/src/main/java/picsou/MainInMemory.java
+++ picsou/picsou/src/main/java/picsou/MainInMemory.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class MainInMemory {
   public static void main(String[] args) throws Exception {
-    System.setProperty(PicsouApplication.APPNAME + ".data.in.memory", "true");
+    System.setProperty(PicsouApplication.IS_DATA_IN_MEMORY, "true");
     System.setProperty(PicsouApplication.DISABLE_IMPORT, "true");
     List<String> strings = new ArrayList<String>(Arrays.asList(args));
     if (!strings.contains("-l")) {
