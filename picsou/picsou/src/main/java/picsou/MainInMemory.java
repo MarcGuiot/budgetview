07c52d032938743b1911ef5a0506a9118d68954b
mguiot <devnull@localhost>
2014-06-30 23:10:27 +0200

    On ne peut pas faire de backup en mode in memory.

--- picsou/picsou/src/main/java/picsou/MainInMemory.java
+++ picsou/picsou/src/main/java/picsou/MainInMemory.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class MainInMemory {
   public static void main(String[] args) throws Exception {
     System.setProperty(PicsouApplication.IS_DATA_IN_MEMORY, "true");
-    System.setProperty(PicsouApplication.DISABLE_IMPORT, "true");
+    System.setProperty(PicsouApplication.DISABLE_BACKUP, "true");
     List<String> strings = new ArrayList<String>(Arrays.asList(args));
     if (!strings.contains("-l")) {
       strings.addAll(Arrays.asList("-l", "fr"));
