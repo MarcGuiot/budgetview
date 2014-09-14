fed13c51b199f8fa5ad870e32d6ef1dfee2bc1d9
rmedina <devnull@localhost>
2014-09-14 18:40:58 +0200

    AddOns: suppression des descriptifs complets

--- picsou/picsou/src/main/java/org/designup/picsou/gui/addons/AddOnsView.java
+++ picsou/picsou/src/main/java/org/designup/picsou/gui/addons/AddOnsView.java
@@ -1,17 +1,14 @@
 package org.designup.picsou.gui.addons;
 
 import org.designup.picsou.gui.View;
-import org.designup.picsou.gui.help.HyperlinkHandler;
 import org.designup.picsou.gui.startup.components.LogoutService;
 import org.designup.picsou.model.AddOns;
 import org.designup.picsou.utils.Lang;
 import org.globsframework.gui.GlobsPanelBuilder;
 import org.globsframework.gui.splits.PanelBuilder;
-import org.globsframework.gui.splits.SplitsNode;
 import org.globsframework.gui.splits.layout.CardHandler;
 import org.globsframework.gui.splits.repeat.RepeatComponentFactory;
 import org.globsframework.gui.splits.utils.Disposable;
-import org.globsframework.gui.splits.utils.GuiUtils;
 import org.globsframework.gui.splits.utils.OnLoadListener;
 import org.globsframework.model.GlobRepository;
 import org.globsframework.model.utils.KeyChangeListener;
@@ -19,17 +16,11 @@ import org.globsframework.utils.directory.Directory;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class AddOnsView extends View {
 
   private JPanel panel = new JPanel();
   private AddOnService addOnService;
-  private AddOn selectedAddOn;
-  private ButtonGroup group = new ButtonGroup();
-  private JEditorPane editor;
-  private JLabel imageLabel = new JLabel();
-  private CardHandler cards;
 
   public AddOnsView(GlobRepository repository, Directory directory) {
     super(repository, directory);
@@ -44,50 +35,21 @@ public class AddOnsView extends View {
 
     builder.addRepeat("addOnSelectors", addOnService.getAddOns(), new AddOnSelectorFactory());
 
-    editor = builder.add("editor", new JEditorPane()).getComponent();
-    editor.addHyperlinkListener(new HyperlinkHandler(directory, directory.get(JFrame.class)));
-    initHtmlEditor(editor);
-
-    builder.add("image", imageLabel);
-
-    cards = builder.addCardHandler("cards");
-
-    builder.add("buy", new BuyAddOnAction());
-    builder.add("gotoDemoAccount", new ShowInDemoAccountAction(directory));
-
     parentBuilder.add("addOnsView", builder);
-
-    builder.addOnLoadListener(new OnLoadListener() {
-      public void processLoad() {
-        group.getElements().nextElement().doClick();
-      }
-    });
-  }
-
-  public static void initHtmlEditor(JEditorPane editor) {
-    GuiUtils.initHtmlComponent(editor);
-    GuiUtils.loadCssResource("/addons/addons.css", editor, AddOnsView.class);
   }
 
   private class AddOnSelectorFactory implements RepeatComponentFactory<AddOn> {
 
     public void registerComponents(PanelBuilder cellBuilder, final AddOn addOn) {
-      JToggleButton toggle = new JToggleButton(addOn.getName());
-      toggle.addActionListener(new ActionListener() {
-        public void actionPerformed(ActionEvent e) {
-          select(addOn);
-        }
-      });
-      final SplitsNode<JToggleButton> addOnNode = cellBuilder.add("addOnSelector", toggle);
-      group.add(toggle);
+
+      cellBuilder.add("label", new JLabel(addOn.getName()));
+      cellBuilder.add("image", new JLabel(addOn.getIcon()));
+
+      final CardHandler cards = cellBuilder.addCardHandler("cards");
 
       final KeyChangeListener updater = new KeyChangeListener(AddOns.KEY) {
         public void update() {
-          boolean enabled = addOn.isEnabled(repository);
-          addOnNode.applyStyle(enabled ? "addOnEnabled" : "addOnDisabled");
-          if (addOn == selectedAddOn) {
-            cards.show(enabled ? "enabled" : "disabled");
-          }
+          cards.show(addOn.isEnabled(repository) ? "enabled" : "disabled");
         }
       };
       repository.addChangeListener(updater);
@@ -96,25 +58,28 @@ public class AddOnsView extends View {
           repository.removeChangeListener(updater);
         }
       });
-      updater.update();
-    }
-  }
 
-  private void select(AddOn addOn) {
-    this.selectedAddOn = addOn;
-    imageLabel.setIcon(addOn.getIcon());
-    editor.setText(addOn.getDescription());
-    editor.setCaretPosition(0);
-    cards.show(addOn.isEnabled(repository) ? "enabled" : "disabled");
+      cellBuilder.add("buy", new BuyAddOnAction(addOn));
+      cellBuilder.add("gotoDemoAccount", new ShowInDemoAccountAction(directory));
+
+      cellBuilder.addOnLoadListener(new OnLoadListener() {
+        public void processLoad() {
+          updater.update();
+        }
+      });
+    }
   }
 
   private class BuyAddOnAction extends AbstractAction {
-    public BuyAddOnAction() {
-      super(Lang.get("addons.buy"));
+    private AddOn addOn;
+
+    public BuyAddOnAction(AddOn addOn) {
+      super(Lang.get("addons.learnMore"));
+      this.addOn = addOn;
     }
 
     public void actionPerformed(ActionEvent e) {
-      selectedAddOn.activate(repository, directory);
+      addOn.activate(repository, directory);
     }
   }
 
@@ -123,11 +88,12 @@ public class AddOnsView extends View {
     private Directory directory;
 
     public ShowInDemoAccountAction(Directory directory) {
-      super(Lang.get("signpostView.gotoDemo.button"));
+      super(Lang.get("addons.example"));
       this.directory = directory;
     }
 
     public void actionPerformed(ActionEvent actionEvent) {
       directory.get(LogoutService.class).gotoDemoAccount();
     }
-  }}
+  }
+}
