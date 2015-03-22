681a758b4db97f5a8959b92649dabc6172ece7d3
rmedina <devnull@localhost>
2015-03-22 15:24:54 +0100

    Suppression du filtrage sur une enveloppe quand elle est supprimee

--- picsou/picsou/src/main/java/org/designup/picsou/gui/transactions/TransactionSelection.java
+++ picsou/picsou/src/main/java/org/designup/picsou/gui/transactions/TransactionSelection.java
@@ -2,24 +2,21 @@ package org.designup.picsou.gui.transactions;
 
 import org.designup.picsou.gui.components.filtering.FilterClearer;
 import org.designup.picsou.gui.components.filtering.FilterManager;
-import org.designup.picsou.model.Account;
-import org.designup.picsou.model.BudgetArea;
-import org.designup.picsou.model.Month;
-import org.designup.picsou.model.Series;
+import org.designup.picsou.gui.transactions.utils.TransactionMatchers;
+import org.designup.picsou.model.*;
+import org.designup.picsou.utils.Lang;
 import org.globsframework.gui.GlobSelection;
 import org.globsframework.gui.GlobSelectionListener;
 import org.globsframework.gui.SelectionService;
 import org.globsframework.gui.utils.GlobSelectionBuilder;
-import org.globsframework.model.GlobList;
-import org.globsframework.model.GlobRepository;
+import org.globsframework.metamodel.GlobType;
+import org.globsframework.model.*;
 import org.globsframework.utils.directory.Directory;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 import static org.designup.picsou.gui.transactions.utils.TransactionMatchers.transactionsForMonths;
+import static org.globsframework.model.utils.GlobMatchers.fieldIn;
 
 public class TransactionSelection implements GlobSelectionListener {
 
@@ -28,9 +25,10 @@ public class TransactionSelection implements GlobSelectionListener {
   private GlobRepository repository;
   private FilterManager filterManager;
   private SelectionService selectionService;
+  private Set<Integer> selectedSeriesIds = new HashSet<Integer>();
 
-  public static final String MONTHS_FILTER = "months";
-  public static final String SERIES_FILTER = "series";
+  private static final String MONTHS_FILTER = "months";
+  private static final String SERIES_FILTER = "series";
 
   public TransactionSelection(FilterManager filterManager, GlobRepository repository, Directory directory) {
     this.filterManager = filterManager;
@@ -41,10 +39,31 @@ public class TransactionSelection implements GlobSelectionListener {
                                  BudgetArea.TYPE, Series.TYPE);
 
     filterManager.addClearer(new SeriesFilterClearer());
+
+    repository.addChangeListener(new ChangeSetListener() {
+      public void globsChanged(ChangeSet changeSet, GlobRepository repository) {
+        if (changeSet.containsDeletions(Series.TYPE)) {
+          Set<Key> deleted = changeSet.getDeleted(Series.TYPE);
+          for (Key key : deleted) {
+            if (selectedSeriesIds.contains(key.get(Series.ID))) {
+              clearSelection();
+              return;
+            }
+          }
+        }
+      }
+
+      public void globsReset(GlobRepository repository, Set<GlobType> changedTypes) {
+        if (changedTypes.contains(Series.TYPE)) {
+          clearSelection();
+        }
+      }
+    });
   }
 
   public void init() {
     currentMonths = Collections.emptySet();
+    selectedSeriesIds.clear();
   }
 
   public void selectionUpdated(GlobSelection selection) {
@@ -55,16 +74,42 @@ public class TransactionSelection implements GlobSelectionListener {
     filterManager.set(MONTHS_FILTER, "", transactionsForMonths(currentMonths));
   }
 
+  public void setTransactionsFilter(GlobList transactions) {
+    String label = transactions.size() == 1 ? Lang.get("filter.transaction.one") : Lang.get("filter.transaction.several", transactions.size());
+    filterManager.set(TransactionSelection.SERIES_FILTER, label, fieldIn(Transaction.ID, transactions.getValueSet(Transaction.ID)));
+    selectedSeriesIds.clear();
+  }
+
+  public void setSeriesFilter(Glob series) {
+    String label = Lang.get("filter.series.single", series.get(Series.NAME));
+    filterManager.set(TransactionSelection.SERIES_FILTER, label, TransactionMatchers.transactionsForSeries(series.get(Series.ID)));
+    selectedSeriesIds.clear();
+    selectedSeriesIds.add(series.get(Series.ID));
+  }
+
+  public void setSeriesFilter(Set<Integer> seriesIds) {
+    String label = seriesIds.size() == 1 ? Lang.get("filter.series.one") : Lang.get("filter.series.several", seriesIds.size());
+    filterManager.set(TransactionSelection.SERIES_FILTER, label, TransactionMatchers.transactionsForSeries(seriesIds));
+    selectedSeriesIds.clear();
+    selectedSeriesIds.addAll(seriesIds);
+  }
+
   private class SeriesFilterClearer implements FilterClearer {
     public List<String> getAssociatedFilters() {
       return Arrays.asList(SERIES_FILTER);
     }
 
     public void clear() {
-      selectionService.select(GlobSelectionBuilder.init()
-                                .add(repository.get(BudgetArea.ALL.getKey()))
-                                .add(GlobList.EMPTY, Series.TYPE)
-                                .get());
+      clearSelection();
     }
+  }
+
+  private void clearSelection() {
+    filterManager.remove(SERIES_FILTER);
+    selectedSeriesIds.clear();
+    selectionService.select(GlobSelectionBuilder.init()
+                              .add(repository.get(BudgetArea.ALL.getKey()))
+                              .add(GlobList.EMPTY, Series.TYPE)
+                              .get());
   }
 }
